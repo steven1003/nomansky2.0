05304305f0755ab2f005727c8a4efde91dd60276
RustyBamboo <dvolya@gmail.com>
2017-01-21 23:52:50 -0500

    Added entities

---
+++ screens/solarsystem/spacemonster/spacemonster.hpp
@@ -0,0 +1,22 @@
+#pragma once
+#include "../sysentity.hpp"
+#include "../spaceplayer.hpp"
+#include "bullet.hpp"
+
+class SpaceMonster : public SysEntity {
+private:
+    sf::RectangleShape shape;
+    SpacePlayer* spacePlayer;
+    std::vector<SysEntity*> *entities;
+    bool fire;
+    float angle;
+    sf::Clock clock;
+    float speed;
+public:
+    SpaceMonster();
+    SpaceMonster(SpacePlayer *spacePlayer);
+    virtual void move();
+    virtual void tick();
+    virtual void draw(sf::RenderWindow &window);
+
+};
