802185686dcbf10506641ba97eb5d8c06acd6837
grymestone <grymestone@ufl.edu>
2017-01-21 23:21:15 -0500

    I choked on my ambition. Again.

--- CMakeFiles/font_font.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-FILE(REMOVE_RECURSE
-  "CMakeFiles/font_font"
-)
-
-# Per-language clean rules from dependency scanning.
-FOREACH(lang)
-  INCLUDE(CMakeFiles/font_font.dir/cmake_clean_${lang}.cmake OPTIONAL)
-ENDFOREACH(lang)
