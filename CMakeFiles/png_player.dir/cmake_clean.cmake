15643f30a42277dca14316ecfa110e9e7ab24e78
grymestone <grymestone@ufl.edu>
2017-01-21 23:32:46 -0500

    Making it pretty.

--- CMakeFiles/png_player.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-FILE(REMOVE_RECURSE
-  "CMakeFiles/png_player"
-)
-
-# Per-language clean rules from dependency scanning.
-FOREACH(lang)
-  INCLUDE(CMakeFiles/png_player.dir/cmake_clean_${lang}.cmake OPTIONAL)
-ENDFOREACH(lang)
